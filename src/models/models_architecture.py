e68f014fd08214e1c2143fabad9af681830b56fc
said-jadli <jadli.said00@gmail.com>
2025-12-14 21:57:28 +0100

    update

--- src/models/models_architecture.py
+++ src/models/models_architecture.py
@@ -1,7 +1,7 @@
 from torch import nn
 import torch.nn.functional as F
 
-
+# SRCNN
 class SRCNN(nn.Module):
     def __init__(self, num_channels=3):
         super(SRCNN, self).__init__()
@@ -20,3 +20,58 @@ class SRCNN(nn.Module):
         x = F.relu(self.conv2(x))
         x = self.conv3(x)
         return x
+    
+
+# EDSR
+
+class ResidualBlock(nn.Module):
+    def __init__(self, channels=64, scale=0.1):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(channels, channels, 3, padding=1),
+            nn.ReLU(True),
+            nn.Conv2d(channels, channels, 3, padding=1)
+        )
+        self.scale = scale
+
+    def forward(self, x):
+        return x + self.block(x) * self.scale
+
+class EDSR(nn.Module):
+    def __init__(self, num_blocks=32, scale_factor=4):
+        super().__init__()
+
+        # 1) Initial Feature Extractor
+        self.conv_head = nn.Conv2d(3, 64, kernel_size=3, padding=1)
+
+        # 2) Residual Blocks
+        self.res_blocks = nn.Sequential(
+            *[ResidualBlock(64) for _ in range(num_blocks)]
+        )
+
+        # 3) Global skip
+        self.conv_tail = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+
+        # 4) Upsampling
+        up_layers = []
+        if scale_factor == 2 or scale_factor == 4:
+            for _ in range(scale_factor // 2):
+                up_layers += [
+                    nn.Conv2d(64, 256, kernel_size=3, padding=1),
+                    nn.PixelShuffle(2),
+                    nn.ReLU(True)
+                ]
+        self.upsample = nn.Sequential(*up_layers)
+
+        # 5) Reconstruction
+        self.conv_last = nn.Conv2d(64, 3, kernel_size=3, padding=1)
+
+    def forward(self, x):
+        x_head = self.conv_head(x)
+        x_res = self.res_blocks(x_head)
+        x = self.conv_tail(x_res) + x_head   
+        x = self.upsample(x)
+        x = self.conv_last(x)
+        return x
+
+
